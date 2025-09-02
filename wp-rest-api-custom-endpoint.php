7df23ac290c60d79aed71bfb5a93bc8187d04d99
id <id@timeless.gr>
2025-09-02 10:41:30 +0300

    Update

--- wp-rest-api-custom-endpoint.php
+++ wp-rest-api-custom-endpoint.php
@@ -3,72 +3,69 @@
 	Plugin Name: REST API Custom Endpoint
 	Description: Add custom endpoint to REST API
 	Version: 1.0.0
-	Author: id
+	Author: Ioakeim Diamantidis
 */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-add_action('rest_api_init', function () {
+add_action( 'rest_api_init', function () {
     register_rest_route(
         'custom/v1',
         '/products',
         array(
             'methods'  => 'POST',
-            'callback' => 'custom_prefix_get_v1_products',
-            // 'permission_callback' => '__return_true',
+            'callback' => 'custom_prefix_get_products',
             'permission_callback' => 'custom_api_bearer_token_check',
         )
     );
 });
 
-function custom_api_bearer_token_check($request)
-{
+function custom_api_bearer_token_check( $request ) {
     $headers = $request->get_headers();
 
-    if (!isset($headers['authorization']) || empty($headers['authorization'])) {
-        return new WP_Error('rest_forbidden', 'Authorization Bearer Token is missing.', array('status' => 401));
+    if ( ! isset( $headers['authorization'] ) || empty( $headers['authorization'] ) ) {
+        return new WP_Error( 'rest_forbidden', 'Authorization Bearer Token is missing.', array( 'status' => 401 ) );
     }
 
     $auth_header = $headers['Authorization'];
 
-    if (preg_match('/Bearer\s(\S+)/', $auth_header, $matches)) {
+    if ( preg_match( '/Bearer\s(\S+)/', $auth_header, $matches ) ) {
         $token = $matches[1];
 
-        // Replace this with your actual validation logic
-        if ($token === 'your-secret-token') {
+        // Validation
+        if ( $token === 'your-secret-token' ) {
             return true;
         }
     }
 
-    return new WP_Error('rest_forbidden', __('Invalid token'), array('status' => 403));
+    return new WP_Error( 'rest_forbidden', __( 'Invalid token' ), array( 'status' => 403 ) );
 }
 
-function custom_prefix_get_v1_products($request)
-{
+function custom_prefix_get_products( $request ) {
     $data = $request->get_params();
 
-    if ((! isset($data['Username']) || ! isset($data['Password'])) || (empty($data['Username']) || empty($data['Password']))) {
-        return new WP_Error('invalid_credentials', 'Invalid username or password.', array('status' => 401));
+    if ( ( ! isset( $data['Username'] ) || ! isset( $data['Password'] ) ) || ( empty( $data['Username'] ) || empty( $data['Password'] ) ) ) {
+        return new WP_Error( 'invalid_credentials', 'Invalid username or password.', array( 'status' => 401 ) );
     }
 
-    $username = sanitize_user($data['Username']);
-    $password = sanitize_text_field($data['Password']);
+    $username = sanitize_user( $data['Username'] );
+    $password = sanitize_text_field( $data['Password'] );
 
     // Check if the user exists
-    $user = get_user_by('login', $username);
+    $user = get_user_by( 'login', $username );
 
-    if (!$user) {
-        return new WP_Error('invalid_user', 'User not found.', array('status' => 401));
+    if ( ! $user ) {
+        return new WP_Error( 'invalid_user', 'User not found.', array( 'status' => 401 ) );
     }
 
     // Verify the password
-    if (!wp_check_password($password, $user->user_pass, $user->ID)) {
-        return new WP_Error('invalid_password', 'Invalid password.', array('status' => 401));
+    if ( ! wp_check_password( $password, $user->user_pass, $user->ID ) ) {
+        return new WP_Error( 'invalid_password', 'Invalid password.', array( 'status' => 401 ) );
     }
 
     // Check if customer
-    if (!in_array('customer', $user->roles)) {
-        return new WP_Error('invalid_user_role', 'User must be customer.', array('status' => 401));
+    if ( ! in_array( 'customer', $user->roles ) ) {
+        return new WP_Error( 'invalid_user_role', 'User must be customer.', array( 'status' => 401 ) );
     }
 
     return 'OK';
